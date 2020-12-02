a7f6684d907b98f173407ad161551b532beae79c
Sadsitha Lokuge <31242200+slokuge@users.noreply.github.com>
2020-12-02 10:39:08 +0100

    Initial commit

---
+++ config.js
@@ -0,0 +1,17 @@
+var REDIRECTS = [
+    {
+        path: "/ipa", iframe: true, title: "IPA", image: "ipa.svg", to: "https://script.google.com/a/macros/etic-insa.com/s/AKfycbxusiq83fptfD3BmIyRhEluPs5h27zNXRjKqHaY9VKDQTrZlvPO/exec/",
+    },
+    {
+        path: "/inscription", iframe: false, title: "Site d'Inscription", to: "https://site-inscription.etic-insa.com/",
+    },
+    {
+        path: "/keros", iframe: false, title: "KEROS", to: "https://keros.etic-insa.com/", 
+    },
+    {
+        path: "/shop", iframe: false, title: "ETIC Shop", to: "https://shop.etic-insa.com/",
+    },
+    {
+        path: "/annuaire", iframe: false, title: "Annuaire des Anciens", to: "https://annuaire-anciens.etic-insa.com/",
+    },
+]
---
+++ script.js
@@ -0,0 +1,42 @@
+function handleNotFound() {
+    var origin = window.location.origin;
+    var infoDiv =  document.getElementById("info");
+    var linkList =  document.createElement("ul");
+    infoDiv.innerText = "";
+    infoDiv.appendChild(linkList);
+
+    REDIRECTS.forEach(redirect => {
+        var linkListItem = document.createElement("li");
+        var link = document.createElement("a");
+        link.innerText =  redirect.title;
+        link.href = origin + redirect.path;
+        linkListItem.appendChild(link);
+        linkList.appendChild(linkListItem);
+    });
+}
+
+function toIframe(redirect) {
+    var body = document.body;
+    body.innerHTML = "";
+    var iframe  = document.createElement("iframe");
+    iframe.src = redirect.to;
+    iframe.id = "iframe";
+    body.appendChild(iframe);
+    document.title = redirect.title;
+}
+
+function initialise() {
+    var pathName = window.location.pathname;
+    REDIRECTS.forEach(redirect => {
+        if (pathName === redirect.path) {
+            if (redirect.iframe) {
+                toIframe(redirect.path);
+            } else {
+                window.location = redirect.path;
+            }
+        }
+    });
+    handleNotFound();
+}
+
+initialise();
