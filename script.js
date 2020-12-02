ffdd3351068edf7d1e749cc892f8a4dc24748595
Sadsitha Lokuge <slokuge97@gmail.com>
2020-12-02 10:47:38 +0100

    Adapting to all paths

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function handleNotFound() {
     var origin = window.location.origin;
-    var infoDiv =  document.getElementById("info");
-    var linkList =  document.createElement("ul");
+    var infoDiv = document.getElementById("info");
+    var linkList = document.createElement("ul");
     infoDiv.innerText = "";
     infoDiv.appendChild(linkList);
 
     REDIRECTS.forEach(redirect => {
         var linkListItem = document.createElement("li");
         var link = document.createElement("a");
-        link.innerText =  redirect.title;
+        link.innerText = redirect.title;
         link.href = origin + redirect.path;
         linkListItem.appendChild(link);
         linkList.appendChild(linkListItem);
@@ -18,7 +18,7 @@ function handleNotFound() {
 function toIframe(redirect) {
     var body = document.body;
     body.innerHTML = "";
-    var iframe  = document.createElement("iframe");
+    var iframe = document.createElement("iframe");
     iframe.src = redirect.to;
     iframe.id = "iframe";
     body.appendChild(iframe);
@@ -26,16 +26,20 @@ function toIframe(redirect) {
 }
 
 function initialise() {
-    var pathName = window.location.pathname;
-    REDIRECTS.forEach(redirect => {
-        if (pathName === redirect.path) {
-            if (redirect.iframe) {
-                toIframe(redirect.path);
-            } else {
-                window.location = redirect.path;
+    try {
+        var path = window.location.pathname.split("/").pop();
+        REDIRECTS.forEach(redirect => {
+            if (path === redirect.path) {
+                if (redirect.iframe) {
+                    toIframe(redirect.path);
+                } else {
+                    window.location = redirect.path;
+                }
             }
-        }
-    });
+        });
+    } catch (error) {
+        window.alert("Redirection error, please use a valid path")
+    }
     handleNotFound();
 }
 
